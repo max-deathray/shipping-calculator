351ef6d9fb2d63a965221debda7ba68f9498088f
mcrae <mpetrey@gmail.com>
2026-02-03 07:05:35 -0500

    Checkpoint: Style cleanup.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Providers from "./providers";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,13 +13,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Providers>{children}</Providers>
         <Analytics />
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import currency from "currency.js";
 import { InputLabel, MenuItem } from "@mui/material";
